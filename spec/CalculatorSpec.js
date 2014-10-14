e811820e657c13ba5a1a6bc8b262f89eb5be79f2
Santu Mahapatra <santu.m@multunus.com>
2014-10-14 12:48:18 +0530

    Checking input for first operand.

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -1,2 +1,17 @@
 describe("Calculator", function() {
+  var calculator;
+
+  beforeEach(function() {
+    calculator = new Calculator();
+  });
+
+  it("throws error message on trying to set operand with wrong format", function () {
+    calculator.setFirstOperand("a");
+    expect(calculator.firstOperand).toBe(undefined);
+  });
+
+  it("sets the operand if operand has integer or float format", function () {
+    calculator.setFirstOperand(2);
+    expect(calculator.firstOperand).toBe(2);
+  });
 });
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,2 +1,9 @@
 function Calculator() {
+  var firstOperand;
+
+  this.setFirstOperand = function(input) {
+    if ( typeof(input) === 'number' ) {
+      this.firstOperand = input;
+    }
+  };
 }
